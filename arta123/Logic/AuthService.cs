953786348d02aed72f6f69cd29fe92b20fbbe8b7
vana138 <ivansadanov960@gmail.com>
2025-01-22 15:27:14 +0300

    Proekt

---
+++ arta123/Logic/AuthService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using arta123.Model;
+
+namespace arta123.Logic
+{
+    public class AuthService
+    {
+        private List<User> _users;
+        public AuthService()
+        {
+            _users = new List<User>
+            {
+                new User("Login", "Pass"),
+                new User("Login1", "Pass1"),
+                new User("Login", "Pass"),
+            };
+
+        }
+
+
+        public bool CheckData(string login, string pass)
+        {
+            var user = _users.FirstOrDefault(u => u.Login == login && u.Pass == pass);
+
+            if (user != null && user.Pass == pass)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+   
+    }
+
+  
+}       
+
---
+++ arta123/authwindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using arta123.Logic;
+
+namespace arta123
+{
+    /// <summary>
+    /// Логика взаимодействия для authwindow.xaml
+    /// </summary>
+    public partial class authwindow : Window
+    {
+        AuthService _authService = new AuthService();
+        
+        public authwindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string login = TbxLogin.Text;
+            string pass = TbxPass.Text;
+
+            if (_authService.CheckData(login,pass))
+            {
+                MainWindow mainwindow = new MainWindow();
+                mainwindow.Show();
+            }
+            else 
+            {
+                MessageBox.Show("Ошибка ввода логина или пароля. Проверьте значения");
+            }
+        }
+    }
+}
